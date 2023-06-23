ed325903c833127018aab3056d8d573d73b0f4d7
HanByul Yang <hanbyul.yang@gmail.com>
2023-06-23 13:17:08 +0900

    half precision model. but no performance gain against fp32 while cuda does

--- example.py
+++ example.py
@@ -3,26 +3,39 @@ import onnxruntime as ort
 import numpy as np
 import time
 
-print(ort.get_available_providers())
 print(ort.__version__)
 
-img = cv2.imread("sample.jpg")
+input_filename = "sample.jpg"
+output_filename = "sample_upscaled.jpg"
+model_name ='realesr-general-x4v3-fp16.onnx'
+
+img = cv2.imread(input_filename)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 input_data_normalized = img.astype(np.float32) / 255.0
 
-sess = ort.InferenceSession('realesr-general-x4v3.onnx', providers=['DmlExecutionProvider'], provider_options=[{'device_id': 1}])
+provider = "DmlExecutionProvider"
+
+if not provider in ort.get_available_providers():
+    print(f"provider {provider} not found in {ort.get_available_providers()}")
+    exit()
+
+providers = [provider]
+
+sess_options = ort.SessionOptions()
+# sess_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_ALL
+sess = ort.InferenceSession(model_name, sess_options=sess_options, providers=providers, provider_options=[{'device_id': 1}])
 input_name = sess.get_inputs()[0].name
 output_name = sess.get_outputs()[0].name
 
-# measure time 
+# measure time onnx
 for i in range(10):
+    input_data_blob = input_data_normalized.transpose(2, 0, 1)[np.newaxis, ...].astype(np.float16)
     start_time = time.time()
-    input_data_blob = input_data_normalized.transpose(2, 0, 1)[np.newaxis, ...]
     output_data_blob = sess.run([output_name], {input_name: input_data_blob})[0]
-    output_img = output_data_blob.squeeze().transpose(1, 2, 0)
     end_time = time.time()
-    print("upscale time: ", end_time - start_time)
+    output_img = output_data_blob.squeeze().transpose(1, 2, 0)
+    print("onnx inference time: ", end_time - start_time)
 
 output_img = (output_img * 255).round().clip(0, 255).astype(np.uint8)
 output_img = cv2.cvtColor(output_img, cv2.COLOR_RGB2BGR)
-cv2.imwrite("sample_upscaled.jpg", output_img)
+cv2.imwrite(output_filename, output_img)
